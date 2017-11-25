0cb3b0d697b1e7183b29d643c34b02d0be2e5a32
verchol <verchol@gmail.com>
2017-11-25 23:23:25 +0200

    fix

--- service1/routes/index.js
+++ service1/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const k8s = require('./k8s')
+//const k8s = require('./k8s')
 const commit = require('./commit')
 /* GET home page. */
 //router.use('/k8', k8s);
